0918ee5a6c6105d7d0ba46e3ad11a84ff94503d7
shantanu-sharechat <shantanusharma@sharechat.co>
2024-06-22 20:22:58 +0530

    fix cmake

--- agora/libagorac/agoraconfig.h
+++ agora/libagorac/agoraconfig.h
@@ -1,12 +1,6 @@
 #ifndef _AGORA_CONFIG_H_
 #define _AGORA_CONFIG_H_
 
-typedef void (*event_fn)(void* userData,
-                         int type,
-                         const char* userName,
-                         long param1,
-                         long param2,
-                         long* states);
 typedef struct{
   char*           app_id;
   char*           ch_id;
