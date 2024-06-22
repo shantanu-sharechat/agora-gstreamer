4d2871e2ab928c20ed5a1e5a8df4bba84caa84a9
shantanu-sharechat <shantanusharma@sharechat.co>
2024-06-22 09:53:42 +0530

    make file fix

--- agora/libagorac/agoraio.h
+++ agora/libagorac/agoraio.h
@@ -18,11 +18,18 @@ class AgoraIO{
   agora::agora_refptr<agora::rtc::IRtcConnection> connection;
   std::shared_ptr<agora::rtc::ILocalUserObserver> local_user_observer;
   std::shared_ptr<YUVFrameObserver> yuvFrameObserver;
+  void AgoraIO::setupAudioFrameObserver();
+  void AgoraIO::setupVideoFrameObserver();
+  void AgoraIO::setupLocalUserObserver();
+  void AgoraIO::setupConnectionObserver();
+  bool AgoraIO::doConnect();
+  void AgoraIO::subscribe();
+  bool AgoraIO::initializeService();
+  bool AgoraIO::createConnection();
 public:
   AgoraIO(agora_config_t* agora_config);
   bool init();
   void setVideoOutFn(agora_media_out_fn fn, void* user_data);
   void setAudioOutFn(agora_media_out_fn fn, void* user_data);
 };
-
 #endif //AGORA_GSTREAMER_AGORAIO_H
