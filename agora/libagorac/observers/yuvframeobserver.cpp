42f1f1a1cdbb27cb6f66affbf7aca5c924964630
shantanu-sharechat <shantanusharma@sharechat.co>
2024-06-24 08:43:30 +0530

    output frame

--- agora/libagorac/observers/yuvframeobserver.cpp
+++ agora/libagorac/observers/yuvframeobserver.cpp
@@ -7,8 +7,20 @@
 
 void YUVFrameObserver::onFrame(const char* channelId, agora::user_id_t remoteUid, const agora::media::base::VideoFrame* frame){
   std::cout<<"Frame received "<< "ts: "<<frame->renderTimeMs<<" width: "<<frame->width<<std::endl;
+  const int len = frame->height * frame->width * 3 / 2;
+  uint8_t* buffer = new uint8_t[len];
+  const int yStride = frame->yStride;
+  const int uStride = frame->uStride;
+  const int vStride = frame->vStride;
+  const int ySize = frame->yStride * frame->height;
+  const int uSize = frame->uStride * frame->height / 2;
+  const int vSize = frame->vStride * frame->height / 2;
+  memccpy((void*)buffer, frame->yBuffer, ySize);
+  memccpy((void*)(buffer + ySize), frame->uBuffer, uSize);
+  memccpy((void*)(buffer + ySize + uSize), frame->vBuffer, vSize);
+
   if(fn){
-//    fn(channelId, remoteUid, frame, user_data);
+    fn(buffer, len, user_data);
   }
 }
 
