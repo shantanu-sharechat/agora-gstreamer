ec7ced023d866e50fa86539382f7c7d404cf1811
shantanu-sharechat <shantanusharma@sharechat.co>
2024-06-26 15:56:57 +0530

    make ts

--- gst-agora/plugin-src/agorasrc/gstagorasrc.c
+++ gst-agora/plugin-src/agorasrc/gstagorasrc.c
@@ -345,6 +345,7 @@ gst_agorasrc_init (Gstagorasrc * agoraSrc)
 
   gst_base_src_set_live (GST_BASE_SRC (agoraSrc), TRUE);
   gst_base_src_set_blocksize  (GST_BASE_SRC (agoraSrc), 10*1024);
+  gst_base_src_set_do_timestamp(GST_BASE_SRC(psrc), true);
 
   //set it initially to null
   agoraSrc->agora_ctx=NULL;
