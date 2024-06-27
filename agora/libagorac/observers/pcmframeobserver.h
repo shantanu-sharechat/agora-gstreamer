338f25140f620475a4a47d6dfa82c73eab51d760
shantanu-sharechat <shantanusharma@sharechat.co>
2024-06-27 12:20:05 +0530

    fixes

--- agora/libagorac/observers/pcmframeobserver.h
+++ agora/libagorac/observers/pcmframeobserver.h
@@ -8,6 +8,7 @@
 #include "AgoraMediaBase.h"
 #include "frameobserver.h"
 #include "agorac.h"
+#include "NGIAgoraAudioTrack.h"
 
 class PcmFrameObserver : public agora::media::IAudioFrameObserverBase, public FrameObserver{
   agora_media_out_fn fn;
@@ -23,19 +24,9 @@ public:
 
   bool onMixedAudioFrame(const char* channelId,AudioFrame& audioFrame) override { return true; };
 
-  bool onPlaybackAudioFrameBeforeMixing(const char* channelId, agora::media::base::user_id_t userId, AudioFrame& audioFrame) override;
+  bool onPlaybackAudioFrameBeforeMixing(const char* channelId, agora::media::base::user_id_t userId, agora::media::AudioFrame& audioFrame) override;
 
   bool onEarMonitoringAudioFrame(AudioFrame& audioFrame) override {return true;};
-
-  AudioParams getEarMonitoringAudioParams()override {return  AudioParams();};
-
-  int getObservedAudioFramePosition() override {return 0;};
-
-  AudioParams getPlaybackAudioParams() override {return  AudioParams();};
-
-  AudioParams getRecordAudioParams()  override {return  AudioParams();};
-
-  AudioParams getMixedAudioParams() override {return  AudioParams();};
 
   void setFrameOutFn(agora_media_out_fn _fn, void* _user_data) override;
 
