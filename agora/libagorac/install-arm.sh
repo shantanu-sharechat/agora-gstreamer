662d59e2bdb01be23b2319cf1e92f47429f14097
shantanu-sharechat <shantanusharma@sharechat.co>
2024-07-04 17:24:58 +0530

    add arm adk

---
+++ agora/libagorac/install-arm.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+INPUT_DIR=$1;
+export AGORA_SDK_DIR=$INPUT_DIR
+
+echo "MAKE"
+make
+echo "MAKE INSTALL"
+sudo make install
+sudo cp $INPUT_DIR/agora_sdk/libagora_rtc_sdk.so  /usr/local/lib
+sudo cp $INPUT_DIR/agora_sdk/libagora-ffmpeg.so  /usr/local/lib
+sudo cp $INPUT_DIR/agora_sdk/libagora-fdkaac.so  /usr/local/lib
+sudo cp agorac.h /usr/local/include
+sudo cp agoraconfig.h /usr/local/include
+sudo ldconfig
