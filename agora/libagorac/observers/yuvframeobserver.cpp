0d9ffb3a88b9790a2c1a02dd95f039b669c5ca2a
shantanu-sharechat <shantanusharma@sharechat.co>
2024-07-05 12:34:44 +0530

    logs

--- agora/libagorac/observers/yuvframeobserver.cpp
+++ agora/libagorac/observers/yuvframeobserver.cpp
@@ -7,13 +7,12 @@
 
 void YUVFrameObserver::onFrame(const char* channelId, agora::user_id_t remoteUid, const agora::media::base::VideoFrame* frame){
 
-  AG_LOG( INFO, "YUVFrameObserver::onFrame ");
   const int ySize = frame->yStride * frame->height;
   const int uSize = frame->uStride * frame->height / 2;
   const int vSize = frame->vStride * frame->height / 2;
   const int len =  ySize + uSize + vSize;
-  AG_LOG( INFO, "YUVFrameObserver::onFrame len %d", len);
   uint8_t* buffer = new uint8_t[len];
+  AG_LOG(INFO, "YUVFrameObserver::onFrame: ySize: %d, uSize: %d, vSize: %d, len: %d, width: %d, height: %d", ySize, uSize, vSize, len, frame->width, frame->height);
   memcpy((void*)buffer, frame->yBuffer, ySize);
   memcpy((void*)(buffer + ySize), frame->uBuffer, uSize);
   memcpy((void*)(buffer + ySize + uSize), frame->vBuffer, vSize);
